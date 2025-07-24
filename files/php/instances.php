e51463ba5083e6ebf306e8970869d28cf5ac83eb
XeroxYTB <huglostalatac@gmail.com>
2025-07-24 22:39:44 +0200

    Update instances.php

--- files/php/instances.php
+++ files/php/instances.php
@@ -1,44 +1,20 @@
 <?php
-$instance['hypixel'] = array_merge($instance['hypixel'], array(
+
+$instance['SausisseSMPv1'] = array_merge($instance['SausisseSMPv1'], array(
     "loadder" => array(
-        "minecraft_version" => "1.8.9",
-        "loadder_type" => "forge",
-        "loadder_version" => "latest"
+        "minecraft_version" => "1.20.1",
+        "loadder_type" => "Forge",
+        "loadder_version" => "1.20.1-47.4.4"
     ),
     "verify" => true,
     "ignored" => array(
         'config',
-        'essential',
-        'logs',
-        'resourcepacks',
-        'saves',
-        'screenshots',
-        'shaderpacks',
-        'W-OVERFLOW',
         'options.txt',
-        'optionsof.txt'
     ),
     "whitelist" => array(),
     "whitelistActive" => false,
     "status" => array(
-        "nameServer" => "Hypixel",
-        "ip" => "mc.hypixel.net",
-        "port" => 25565
-    )
-));
-
-$instance['PokeMoonX'] = array_merge($instance['PokeMoonX'], array(
-    "loadder" => array(
-        "minecraft_version" => "1.16.5",
-        "loadder_type" => "forge",
-        "loadder_version" => "1.16.5-36.2.35"
-    ),
-    "verify" => false,
-    "ignored" => array(),
-    "whitelist" => array(),
-    "whitelistActive" => false,
-    "status" => array(
-        "nameServer" => "PokeMoonX",
+        "nameServer" => "SausisseSMP",
         "ip" => "178.32.106.234",
         "port" => 25599
     )
