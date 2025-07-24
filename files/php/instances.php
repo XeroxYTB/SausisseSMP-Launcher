a92a8a692c2ab047be03b5555e49ca4f164d5d02
XeroxYTB <huglostalatac@gmail.com>
2025-07-25 00:05:13 +0200

    Update instances.php

--- files/php/instances.php
+++ files/php/instances.php
@@ -1,15 +1,19 @@
 <?php
 
-$instance['sausisseSMPv1'] = array_merge($instance['sausisseSMPv1'], array(
+$instance['SausisseSMP'] = array_merge($instance['SausisseSMP'], array(
     "loadder" => array(
         "minecraft_version" => "1.20.1",
-        "loadder_type" => "Forge",
-        "loadder_version" => "47.4.4"
+        "loadder_type" => "forge",
+        "loadder_version" => "1.20.1-47.4.4"
     ),
-    "verify" => true,
+    "verify" => false,
     "ignored" => array(
         'config',
-        'options.txt',
+        'logs',
+        'saves',
+        'screenshots',
+        'shaderpacks',
+        'options.txt'
     ),
     "whitelist" => array(),
     "whitelistActive" => false,
